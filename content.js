411942183f9386e34c06d3b63b64ef2c66fba3b1
Matt Colyer <mcolyer@github.com>
2015-05-22 10:11:11 -0700

    Add a sidebar

--- content.js
+++ content.js
@@ -1,17 +1,11 @@
 InboxSDK.load('1', 'sdk_gh-experiment_b2c055d57d').then(function(sdk){
 
-  // the SDK has been loaded, now do something with it!
-  sdk.Compose.registerComposeViewHandler(function(composeView){
-
-    // a compose view has come into existence, do something with it!
-    composeView.addButton({
-      title: "My Nifty Button!",
-      iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
-      onClick: function(event) {
-        event.composeView.insertTextIntoBodyAtCursor('Hello World!');
-      },
+  sdk.Conversations.registerThreadViewHandler(function(threadView){
+    el = document.createElement("div");
+    el.innerHTML = "Hello World";
+    threadView.addSidebarContentPanel({
+      el: el,
+      title: "Hello World",
     });
-
   });
-
 });
